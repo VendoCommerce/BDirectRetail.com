23f847fe71a69c0df26863a3263e544a21d50c59
CONVERSION\Ravi <CONVERSION\Ravi>
2013-09-27 16:10:51 +0000

    Https Redirect Fix

--- Website/CSWeb/Services.aspx.cs
+++ Website/CSWeb/Services.aspx.cs
@@ -18,20 +18,20 @@ namespace CSWeb.Root.Store
                 base.Page_Load(sender, e);
             SitePreference sitePrefCache = CSFactory.GetCacheSitePref();
      
-            if (Request.Headers["X-HTTPS"] != null)
-            {
-                if (Request.Headers["X-HTTPS"].ToLower().Equals("no"))
-                {
-                    if (Request.Url.ToString().Contains("www"))
-                    {
-                        Response.Redirect((Request.Url.ToString().Replace("http:/", "https:/").Replace("index.aspx", "")));
-                    }
-                    else
-                    {
-                        Response.Redirect((Request.Url.ToString().Replace("http:/", "https:/").Replace("https://", "https://www.").Replace("index.aspx", "")));
-                    }
-                }
-            }
+            //if (Request.Headers["X-HTTPS"] != null)
+            //{
+            //    if (Request.Headers["X-HTTPS"].ToLower().Equals("no"))
+            //    {
+            //        if (Request.Url.ToString().Contains("www"))
+            //        {
+            //            Response.Redirect((Request.Url.ToString().Replace("http:/", "https:/").Replace("index.aspx", "")));
+            //        }
+            //        else
+            //        {
+            //            Response.Redirect((Request.Url.ToString().Replace("http:/", "https:/").Replace("https://", "https://www.").Replace("index.aspx", "")));
+            //        }
+            //    }
+            //}
                 
             }
            
